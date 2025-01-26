709775ff9ce636a9a31f486601eacb3ef1ace049
Prince N. <nwaekwuprince@gmail.com>
2025-01-26 22:44:07 +0100

    update main terraform file

--- main.tf
+++ main.tf
@@ -8,13 +8,14 @@ terraform {
 }
 
 provider "google" {
-  project     = "placeng"
-  region      = "europe-west1"
+  credentials = file(var.credentials)
+  project     = var.project
+  region      = var.region
 }
 
 resource "google_storage_bucket" "test-bucket" {
-  name          = "placeng-test-bucket-eu"
-  location      = "EU"
+  name          = var.gcs_bucket_name
+  location      = var.location
   force_destroy = true
 
   lifecycle_rule {
@@ -25,4 +26,9 @@ resource "google_storage_bucket" "test-bucket" {
       type = "AbortIncompleteMultipartUpload"
     }
   }
+}
+
+resource "google_bigquery_dataset" "test_dataset" {
+  dataset_id = var.bq_dataset_name
+  location = var.location
 }
